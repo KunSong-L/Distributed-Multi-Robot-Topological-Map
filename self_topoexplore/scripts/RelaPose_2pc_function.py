e48bc2fa45d9a13bf08edb676196f2652a99384e
Kun Song <coldtea@sjtu.edu.cn>
2023-05-11 17:09:08 +0800

    add relative pose estimation part

--- self_topoexplore/scripts/RelaPose_2pc_function.py
+++ self_topoexplore/scripts/RelaPose_2pc_function.py
@@ -4,6 +4,12 @@ import numpy as np
 import math
 import cv2 as cv
 import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.spatial.transform import Rotation as R
+import math
+from sklearn.cluster import DBSCAN
+from scipy import stats
 
 def ransac(data, fit_model, distance_func, sample_size, max_distance, guess_inliers):
     #fit_model(data): input:data;  output:model
@@ -24,9 +30,10 @@ def ransac(data, fit_model, distance_func, sample_size, max_distance, guess_inli
 
         now_model = fit_model(sub_data)
         now_dis = distance_func(data,now_model)
-        inlier_index1 = now_dis[:,0]<max_distance[0]
-        inlier_index2 = now_dis[:,1]<max_distance[1]
-        inlier_index = inlier_index1 * inlier_index2
+        inlier_index = now_dis[:,0]<max_distance[0]
+        for i in range(1,len(max_distance)):
+            inlier_index = inlier_index * (now_dis[:,i]<max_distance[i])
+            
         inlier_num = np.sum(inlier_index)
         
         if inlier_num > best_inlier_num:
@@ -112,10 +119,7 @@ def Fast_Robutst_2PC(pc1,pc2):
 
     return theta,phi
 
-
-def planar_motion_calcu(img1,img2,k1,k2):
-    method = 1
-    show_img = 1
+def planar_motion_calcu_single(img1,img2,k1,k2,method=1):
     use_knn_matcher = 1
     if method == 0:
         # Initiate ORB detector
@@ -146,12 +150,12 @@ def planar_motion_calcu(img1,img2,k1,k2):
             bf = cv.BFMatcher(crossCheck=True)
             matches = bf.match(des1,des2)
 
-    if show_img:
-        img3 = cv.drawMatches(img1,kp1,img2,kp2,matches,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-        plt.imshow(img3),plt.show()
-
     # print("good match num = ", len(matches))      
     match_point_num = len(matches)
+
+    if match_point_num < 10:
+        return [], []
+
     matchedPoints1 = np.ones((3,match_point_num),float)
     matchedPoints2 = np.ones((3,match_point_num),float)
 
@@ -159,6 +163,10 @@ def planar_motion_calcu(img1,img2,k1,k2):
         now_match_point = matches[i]
         matchedPoints1[0:2,i] = np.array(kp1[now_match_point.queryIdx].pt)
         matchedPoints2[0:2,i] = np.array(kp2[now_match_point.trainIdx].pt)
+
+    sort_index = np.argsort(matchedPoints1[0, :])
+    matchedPoints1 = matchedPoints1[:,sort_index]
+    matchedPoints2 = matchedPoints2[:,sort_index]
 
     match_point_cam_frame_1 = np.linalg.inv(k1) @ matchedPoints1  
     match_point_cam_frame_2 = np.linalg.inv(k2) @ matchedPoints2  
@@ -169,8 +177,8 @@ def planar_motion_calcu(img1,img2,k1,k2):
     total_angleT= np.array([],float)
     #测试过代码正确，就是特征匹配地方效果太差了
     half_len = match_point_num//2
-
-    for i in range(0,half_len,2):
+    # calculate relative pose
+    for i in range(0,half_len):
         #选择两个
         #按照下标直接选择效果比较差，需要重新选择一下顺序
         #建议这部分做一些误差分析，做一点理论研究
@@ -180,57 +188,211 @@ def planar_motion_calcu(img1,img2,k1,k2):
                         [match_point_cam_frame_1[0,index_2],match_point_cam_frame_1[1,index_2]]])
         P_j = np.array([[match_point_cam_frame_2[0,index_1],match_point_cam_frame_2[1,index_1]], 
                         [match_point_cam_frame_2[0,index_2],match_point_cam_frame_2[1,index_2]]])
+        
         # P_i = match_point_cam_frame_1[0:2,i:i+2].T
         # P_j = match_point_cam_frame_2[0:2,i:i+2].T
 
         [Yaw, AngleT] = Fast_Robutst_2PC(P_i,P_j)#theta：旋转角度；phi：位移角度
-        # print(match_point_cam_frame_1[0:2,i:i+2])
-        # print(match_point_cam_frame_2[0:2,i:i+2])
+
         total_yaw = np.append(total_yaw,Yaw.T)
         total_angleT = np.append(total_angleT,AngleT.T)
 
 
     total_yaw = total_yaw/np.pi*180
     total_angleT = total_angleT/np.pi*180
-    total_yaw = remap2T(total_yaw,-180,180)
-    total_angleT = remap2T(total_angleT,-180,180)
-    # 旋转角度上具有方向的不确定性，所以把相差一个负号的方向作为一个考虑
-    # index = total_angleT<0
-    # total_angleT[index] = total_angleT[index] + 180
-
-    data = np.concatenate((total_yaw.reshape(-1,1),total_angleT.reshape(-1,1)),axis=1)
-
-    sample_size = 2
-    maxDistance = [5,5]
-    best_model, best_inlier_index, inter_num = ransac(data,fitLineFcn,evalLineFcn,sample_size,maxDistance,guess_inliers = 0.4)
+    
 
     #return yaw and translation angle in degree
-    return best_model
+    return total_yaw, total_angleT
 
-if __name__ == '__main__':
-    img1 = cv.imread("/home/master/debug/robot1.jpg")
-    img2 = cv.imread("/home/master/debug/robot2.jpg")
-    # img1 = cv.cvtColor(img1,cv.COLOR_BGR2GRAY)
-    # img2 = cv.cvtColor(img2,cv.COLOR_BGR2GRAY)
-    cam_rotation = [[0,0,0],[-0.2,0.1,math.pi/2],[-0.4,0.0,math.pi],[-0.2,-0.1,-math.pi/2]]
+
+def planar_motion_calcu_mulit(img1,img2,k1,k2,cam_pose):
+    #cam pose:[[x,y,yaw],...]
+    show_img = 1
+    cam_num = len(cam_pose)
     height = img1.shape[0]
     widht = img1.shape[1]//4
     img1_list = []
     img2_list = []
-    for i in range(4):
+    for i in range(cam_num):
         img1_tmp = img1[0:height,i*widht:(i+1)*widht]
         img2_tmp = img2[0:height,i*widht:(i+1)*widht]
 
         img1_list.append(img1_tmp)
         img2_list.append(img2_tmp)
+        
+    cam_R = []
+    cam_t = []
+    for pose in cam_pose:
+        cam_R.append(R.from_euler('z',pose[2],degrees=False).as_matrix())
+        cam_t.append(np.array([[pose[0]],[pose[1]],[0]]))
+       
+    total_yaw =np.array([],float)
+    total_angleT= np.array([],float)
+    total_ij = np.array([],int).reshape((2,-1))
 
+    for i in range(cam_num):
+        for j in range(cam_num):
+            camera_offset = (cam_pose[i][2] - cam_pose[j][2])/math.pi*180
+            image_now_1 = img1_list[i]
+            image_now_2 = img2_list[j]
 
+            [Yaw, AngleT] = planar_motion_calcu_single(image_now_1,image_now_2,k1,k2,method=1) # use sift FP
+
+            if len(Yaw) == 0:
+                continue
+            Yaw -= camera_offset
+            total_yaw  = np.concatenate((total_yaw,Yaw))
+            total_angleT = np.concatenate((total_angleT,AngleT))
+
+            tmp_ij = np.zeros((2,len(Yaw)),int)
+            tmp_ij[0,:] = i
+            tmp_ij[1,:] = j
+            total_ij  = np.append(total_ij,tmp_ij,axis = 1)
+    
+    
+    total_yaw = remap2T(total_yaw,-180,180)
+    total_angleT = remap2T(total_angleT,-180,180)
+
+    sample_size = 3
+    maxDistance = [5]
+    yaw_result, best_inlier_index, inter_num = ransac(total_yaw.reshape((-1,1)),fitLineFcn,evalLineFcn,sample_size,maxDistance,guess_inliers = 0.4)
+
+    t1 = []
+    t2 = []
+    rho_index = []
+
+    R_robot = R.from_euler('z', -yaw_result, degrees=True).as_matrix().reshape(3,3)
+    count = 0
+    now_i = total_ij[0,0]
+    now_j = total_ij[1,0]
+    for i in range(len(best_inlier_index)):
+        if best_inlier_index[i] != True:
+            continue
+        R_i = cam_R[total_ij[0,i]]
+        t_i = cam_t[total_ij[0,i]]
+        R_j = cam_R[total_ij[1,i]]
+        t_j = cam_t[total_ij[1,i]]
+        tmp_angle = total_angleT[i]
+        now_phi = total_angleT[i]/180*math.pi
+        t_cam = np.array([[np.cos(now_phi)], [-np.sin(now_phi)], [0]])
+
+        t1.append(R_i@t_cam)
+        t2.append(t_i - R_robot @ t_j)
+
+        if now_i != total_ij[0,i] or now_j != total_ij[1,i]:
+            now_i = total_ij[0,i]
+            now_j = total_ij[1,i]
+            count += 1
+        rho_index.append(count)
+
+    if show_img:
+        index  = np.arange(0,len(best_inlier_index))
+        plt.scatter(index[best_inlier_index],total_yaw[best_inlier_index],s=2)
+        plt.scatter(index[np.logical_not(best_inlier_index)],total_yaw[np.logical_not(best_inlier_index)],c = 'r',s=2)
+        plt.title("yaw inlier and outlier")
+        plt.show()
+
+    #return yaw and translation angle in degree
+    return yaw_result,t1,t2,rho_index
+
+if __name__=="__main__":
+    img1 = cv.imread("/home/master/debug/robot1.jpg")
+    img2 = cv.imread("/home/master/debug/robot2.jpg")
+    img1 = cv.cvtColor(img1,cv.COLOR_BGR2GRAY)
+    img2 = cv.cvtColor(img2,cv.COLOR_BGR2GRAY)
+    x_offset = 0.1
+    y_offset = 0.2
+    cam_trans = [[x_offset,0,0],[0,y_offset,math.pi/2],[-x_offset,0.0,math.pi],[0,-y_offset,-math.pi/2]]
+    cam_R = []
+    cam_t = []
+    for pose in cam_trans:
+        cam_R.append(R.from_euler('z',pose[2],degrees=False).as_matrix())
+        cam_t.append(np.array([[pose[0]],[pose[1]],[0]]))
 
     # read K
     K1_mat=np.array([319.9988245765257, 0.0, 320.5, 0.0, 319.9988245765257, 240.5, 0.0, 0.0, 1.0]).reshape((3,3))
     K2_mat=np.array([319.9988245765257, 0.0, 320.5, 0.0, 319.9988245765257, 240.5, 0.0, 0.0, 1.0]).reshape((3,3))
-    # plt.imshow(img1_list[0],cmap=plt.get_cmap('gray'))
-    # plt.show()
-    best_model = planar_motion_calcu(img1_list[0],img2_list[1],K1_mat,K2_mat)
-    # best_model = planar_motion_calcu(img1,img2,K1_mat,K2_mat)
+
+    cam_index = [1,2,1,1]
+    # cam_index = [1,2,2,2]
+    best_model,t1,t2,rho_index = planar_motion_calcu_mulit(img1,img2,K1_mat,K2_mat,cam_trans)
+    print(best_model[0][0])
+    plt.figure()
+    rho = np.arange(-2,2,0.1)
+    x_plot = np.ones(len(rho),float)
+    y_plot = np.ones(len(rho),float)
+    for j in range(0,len(t1)):
+        for i in range(len(rho)):
+            now_t1 = t1[j][0:2]
+            now_t2 = t2[j][0:2]
+            tmp = rho[i] * now_t1 + now_t2
+            x_plot[i] = tmp[0,0]
+            y_plot[i] = tmp[1,0]
+        plt.plot(x_plot,y_plot)
+    plt.xlim((-1,1))
+    plt.ylim((-2,2))
+
+    #direction solve
+    interextion_line = np.array([],float).reshape((2,-1))
+    for i in range(0,len(t1)):
+        for j in range(i,len(t1)):
+            if rho_index[i]==rho_index[j]:
+                continue
+            first_t1 = t1[i][0:2]
+            first_t2 = t2[i][0:2]
+
+            second_t1 = t1[j][0:2]
+            second_t2 = t2[j][0:2]
+
+            e1 = first_t1
+            e2 = -second_t1
+
+            #三个向量都在平面上
+            A = np.array([[e1[0][0],e2[0][0]],[e1[1][0], e2[1][0]]])
+            b = (second_t2 - first_t2)[0:2]
+            try:
+                res = np.linalg.inv(A) @ b
+                rho1 = res[0][0]
+                rho2 = res[1][0]
+                if abs(rho1)>10 or abs(rho2)>10:
+                    continue
+                res = rho1*first_t1 + first_t2
+                interextion_line = np.append(interextion_line,res,axis = 1)
+            except:
+                continue
     
+    # cluster
+    dbscan = DBSCAN(eps=0.02, min_samples=20).fit(interextion_line.T)#聚类
+    point_lable = dbscan.labels_
+    lables = np.unique(dbscan.labels_)# 获取有几类
+    mode, count = stats.mode(dbscan.labels_)
+    if mode[0]==-1:
+        most_common_label = mode[1]
+        num_most_common = count[1]
+    else:
+        most_common_label = mode[0]
+        num_most_common = count[0]
+    print("most common label is:  ", most_common_label)
+    print("number of common label is:  ", num_most_common)
+    points_list = [np.array([],float).reshape((2,-1)) for i in range(len(lables))]#获取每一类具体有多少点
+
+    for i in range(interextion_line.shape[1]):
+        points_list[dbscan.labels_[i]] = np.append(points_list[dbscan.labels_[i]],interextion_line[:,i].reshape(2,-1),axis = 1) #把每个点加进对应类里面去
+    
+    plt.figure()
+    plt.scatter(points_list[most_common_label][0,:],points_list[most_common_label][1,:],s=1)
+
+    plt.figure()
+    for i in range(len(lables)):
+        plt.scatter(points_list[i][0,:],points_list[i][1,:],s=1)
+    plt.show()
+    print("estimated translation is: x = ",np.mean(points_list[2][0,:]),"   y= ",np.mean(points_list[2][1,:]))
+
+
+
+    
+
+
+
+
